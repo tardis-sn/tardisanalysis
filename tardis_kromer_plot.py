481d132c3d7229e888558d899761e06aabb3e8f6
Ulrich Noebauer <ulrich.noebauer@gmail.com>
2016-02-04 17:05:43 +0100

    replaced main plotting routine with a plotter class
    * plotter class has same plotting functionally as replaced routine
    * currently undocumented
    * not yet PEP8 compliant

--- tardis_kromer_plot.py
+++ tardis_kromer_plot.py
@@ -134,155 +134,324 @@ class model_h5(object):
         self.time_of_simulation = mhdf5["/configuration/"]["time_of_simulation"]
 
 
-def make_kromer_plot_virt(mdl, bins=None, cmap=cm.jet, ax=None, xlim=None,
-                          ylim=None, twinx=True):
+class tardis_kromer_plotter(object):
+    def __init__(self, mdl):
 
-    # Safety checks
-    try:
-        assert(type(mdl) == tardis.model.Radial1DModel or type(mdl) == model_h5)
-    except AssertionError:
-        raise ValueError("'mdl' must be either a model_h5 or a"
-                         " tardis.model.Radia1DModel instance")
+        self._mdl = None
+        self._zmax = 32
+        self._cmap = cm.jet
+        self._xlim = None
+        self._ylim = None
+        self._twinx = False
 
-    if mdl.runner.virt_logging == 0:
-        raise ValueError("Virtual packet logging was deactivated in the input "
-                         "model. Cannot generate Kromer plot")
+        self._bins = None
+        self._ax = None
+        self._pax = None
 
-    zmax = 32
+        self._noint_mask = None
+        self._escat_mask = None
+        self._escatonly_mask = None
+        self._line_mask = None
+        self._lam_escat = None
+        self._lam_noint = None
+        self._weights_escat = None
+        self._weights_noint = None
+        self._line_in_infos = None
+        self._line_in_nu = None
+        self._line_in_L = None
+        self._line_out_infos = None
+        self._line_out_nu = None
+        self._line_out_L = None
 
-    if ax is None:
-        ax = plt.figure().add_subplot(111)
+        self.mdl = mdl
 
-    if bins is None:
-        bins = mdl.spectrum_virtual.wavelength[::-1]
+    @property
+    def mdl(self):
+        return self._mdl
 
-    noint_mask = mdl.runner.virt_packet_last_interaction_type == -1
-    escat_mask = mdl.runner.virt_packet_last_interaction_type == 1
-    escatonly_mask = \
-        ((mdl.runner.virt_packet_last_line_interaction_in_id == -1) *
-         (escat_mask)).astype(np.bool)
+    @mdl.setter
+    def mdl(self, val):
+        try:
+            assert(type(val) == tardis.model.Radial1DModel or type(val) ==
+                   model_h5)
+        except AssertionError:
+            raise ValueError("'mdl' must be either a model_h5 or a"
+                             " tardis.model.Radia1DModel instance")
 
-    lam_noint = (csts.c.cgs / (mdl.runner.virt_packet_nus[noint_mask] *
-                               units.Hz)).to(units.AA)
-    lam_escat = (csts.c.cgs / (mdl.runner.virt_packet_nus[escatonly_mask] *
-                               units.Hz)).to(units.AA)
+        if val.runner.virt_logging == 0:
+            raise ValueError("Virtual packet logging was deactivated in the"
+                             " input model. Cannot generate Kromer plot")
+        self._mdl = val
 
-    weights_noint = (mdl.runner.virt_packet_energies[noint_mask] * units.erg /
-                     mdl.time_of_simulation)
-    weights_escat = (mdl.runner.virt_packet_energies[escatonly_mask] *
-                     units.erg / mdl.time_of_simulation)
+    @property
+    def zmax(self):
+        return self._zmax
 
-    line_mask = ((mdl.runner.virt_packet_last_interaction_type > -1) *
-                 (mdl.runner.virt_packet_last_line_interaction_in_id > -1))
+    @property
+    def cmap(self):
+        return self._cmap
 
-    lams = [lam_noint, lam_escat]
-    weights = [weights_noint, weights_escat]
-    colors = ["black", "grey"]
+    @property
+    def ax(self):
+        return self._ax
+    @property
+    def pax(self):
+        return self._pax
 
-    ids = mdl.runner.virt_packet_last_line_interaction_out_id[line_mask]
-    line_infos = mdl.atom_data.lines.iloc[ids]
+    @property
+    def bins(self):
+        return self._bins
 
-    line_nu = mdl.runner.virt_packet_nus[line_mask]
-    line_L = mdl.runner.virt_packet_energies[line_mask]
+    @property
+    def xlim(self):
+        return self._xlim
 
-    for zi in xrange(1, zmax+1):
-        mask = line_infos.atomic_number.values == zi
-        lams.append((csts.c.cgs / (line_nu[mask] * units.Hz)).to(units.AA))
-        weights.append(line_L[mask] * units.erg / mdl.time_of_simulation)
-        colors.append(cmap(float(zi) / float(zmax)))
+    @property
+    def ylim(self):
+        return self._ylim
 
-    Lnorm = 0
-    for w in weights:
-        Lnorm += np.sum(w)
+    @property
+    def twinx(self):
+        return self._twinx
 
-    ret = ax.hist(lams, bins=bins, stacked=True, histtype="stepfilled",
-                  normed=True, weights=weights)
+    @property
+    def noint_mask(self):
+        if self._noint_mask is None:
+            self._noint_mask = \
+                self.mdl.runner.virt_packet_last_interaction_type == -1
+        return self._noint_mask
 
-    for i, col in enumerate(ret[-1]):
-        for reti in col:
-            reti.set_facecolor(colors[i])
-            reti.set_edgecolor(colors[i])
-            reti.set_linewidth(0)
-            reti.xy[:, 1] *= Lnorm
+    @property
+    def escat_mask(self):
+        if self._escat_mask is None:
+            self._escat_mask = \
+                self.mdl.runner.virt_packet_last_interaction_type == 1
+        return self._escat_mask
 
-    ax.plot(mdl.spectrum_virtual.wavelength,
-            mdl.spectrum_virtual.luminosity_density_lambda, color="blue",
-            drawstyle="steps-post", lw=0.5)
+    @property
+    def escatonly_mask(self):
+        if self._escatonly_mask is None:
+            self._escatonly_mask = \
+                ((self.mdl.runner.virt_packet_last_line_interaction_in_id == -1) \
+                 * (self.escat_mask)).astype(np.bool)
+        return self._escatonly_mask
 
-    if ylim is None:
-        ax.autoscale(axis="y")
-    else:
-        ax.set_ylim(ylim)
+    @property
+    def line_mask(self):
+        if self._line_mask is None:
+            self._line_mask = \
+                ((self.mdl.runner.virt_packet_last_interaction_type > -1) *
+                 (self.mdl.runner.virt_packet_last_line_interaction_in_id > -1))
+        return self._line_mask
 
-    ylim = ax.get_ylim()
+    @property
+    def lam_noint(self):
+        if self._lam_noint is None:
+            self._lam_noint = (csts.c.cgs /
+                               (self.mdl.runner.virt_packet_nus[self.noint_mask]
+                                * units.Hz)).to(units.AA)
+        return self._lam_noint
 
-    if xlim is None:
-        ax.autoscale(axis="x")
-    else:
-        ax.set_xlim(xlim)
+    @property
+    def lam_escat(self):
+        if self._lam_escat is None:
+            self._lam_escat = \
+                (csts.c.cgs /
+                 (self.mdl.runner.virt_packet_nus[self.escatonly_mask] *
+                  units.Hz)).to(units.AA)
+        return self._lam_escat
 
-    xlim = ax.get_xlim()
+    @property
+    def weights_escat(self):
+        if self._weights_escat is None:
+            self._weights_escat = \
+                (self.mdl.runner.virt_packet_energies[self.escatonly_mask] *
+                 units.erg / self.mdl.time_of_simulation)
+        return self._weights_escat
 
-    custcmap = matplotlib.colors.ListedColormap([cmap(float(i) / float(zmax))
-                                                 for i in xrange(1, zmax+1)])
-    bounds = np.arange(zmax+1) + 0.5
-    norm = matplotlib.colors.Normalize(vmin=1, vmax=zmax+1)
-    mappable = cm.ScalarMappable(norm=norm, cmap=custcmap)
-    mappable.set_array(np.linspace(1, zmax + 1, 256))
-    labels = [inv_elements[zi].capitalize() for zi in xrange(1, zmax+1)]
+    @property
+    def weights_noint(self):
+        if self._weights_noint is None:
+            self._weights_noint = \
+                (self.mdl.runner.virt_packet_energies[self.noint_mask] *
+                 units.erg / self.mdl.time_of_simulation)
+        return self._weights_noint
 
-    mainax = ax
-    cbar = plt.colorbar(mappable, ax=mainax)
-    cbar.set_ticks(bounds)
-    cbar.set_ticklabels(labels)
+    @property
+    def line_out_infos(self):
+        if self._line_out_infos is None:
+            ids = self.mdl.runner.virt_packet_last_line_interaction_out_id[self.line_mask]
+            self._line_out_infos = self.mdl.atom_data.lines.iloc[ids]
+        return self._line_out_infos
 
-    if twinx:
-        pax = ax.twinx()
-    else:
-        pax = ax
+    @property
+    def line_out_nu(self):
+        if self._line_out_nu is None:
+            self._line_out_nu = self.mdl.runner.virt_packet_nus[self.line_mask]
+        return self._line_out_nu
 
-    nus = mdl.runner.virt_packet_last_interaction_in_nu * units.Hz
+    @property
+    def line_out_L(self):
+        if self._line_out_L is None:
+            self._line_out_L = self.mdl.runner.virt_packet_energies[self.line_mask]
+        return self._line_out_L
 
-    line_L = mdl.runner.virt_packet_energies[line_mask]
-    line_nu = nus[line_mask]
+    @property
+    def line_in_infos(self):
+        if self._line_in_infos is None:
+            ids = self.mdl.runner.virt_packet_last_line_interaction_in_id[self.line_mask]
+            self._line_in_infos = self.mdl.atom_data.lines.iloc[ids]
+        return self._line_in_infos
 
-    lams = []
-    weights = []
-    colors = []
+    @property
+    def line_in_nu(self):
+        if self._line_in_nu is None:
+            nus = self.mdl.runner.virt_packet_last_interaction_in_nu * units.Hz
+            self._line_in_nu = nus[self.line_mask]
+        return self._line_in_nu
 
-    for zi in xrange(1, zmax+1):
-        mask = line_infos.atomic_number.values == zi
-        lams.append((csts.c.cgs / line_nu[mask]).to(units.AA))
-        weights.append(line_L[mask] * units.erg / mdl.time_of_simulation)
-        colors.append(cmap(float(zi) / float(zmax)))
+    @property
+    def line_in_L(self):
+        if self._line_in_L is None:
+            self._line_in_L = self.mdl.runner.virt_packet_energies[self.line_mask]
+        return self._line_in_L
 
-    Lnorm = 0
-    for w in weights:
-        Lnorm -= np.sum(w)
+    def generate_plot(self, ax=None, cmap=cm.jet, bins=None, xlim=None, ylim=None, twinx=False):
 
-    ret = pax.hist(lams, bins=bins, stacked=True, histtype="stepfilled",
-                   normed=True, weights=weights)
+        self._ax = None
+        self._pax = None
 
-    for i, col in enumerate(ret[-1]):
-        for reti in col:
-            reti.set_facecolor(colors[i])
-            reti.set_edgecolor(colors[i])
-            reti.set_linewidth(0)
-            reti.xy[:, 1] *= Lnorm
+        self._cmap = cmap
+        self._ax = ax
+        self._xlim = xlim
+        self._ylim = ylim
+        self._twinx = twinx
 
-    if twinx:
-        pax.set_ylim([-ylim[-1], -ylim[0]])
-    else:
-        pax.set_ylim([-ylim[-1], ylim[-1]])
-    pax.set_xlim(xlim)
+        if bins is None:
+            self._bins = self.mdl.spectrum_virtual.wavelength[::-1]
+        else:
+            self._bins = bins
 
-    if not twinx:
-        ax.axhline(y=0, color="white", ls="dashed", dash_capstyle="round", lw=2)
+        self._axes_handling_preparation()
+        self._generate_emission_part()
+        self._generate_and_add_colormap()
+        self._generate_and_add_legend()
+        self._paxes_handling_preparation()
+        self._generate_absorption_part()
+        self._axis_handling_label_rescale()
 
-    ax.set_xlabel(r"$\lambda$ [\AA]")
-    ax.set_ylabel(r"$L_{\mathrm{\lambda}}$ [$\mathrm{erg\,s^{-1}\,\AA^{-1}}$]")
+    def _axes_handling_preparation(self):
 
-    bpatch = patches.Patch(color="black", label="photosphere")
-    gpatch = patches.Patch(color="grey", label="e-scattering")
-    bline = lines.Line2D([], [], color="blue", label="virtual spectrum")
-    ax.legend(handles=[bline, gpatch, bpatch])
+        if self._ax is None:
+            self._ax = plt.figure().add_subplot(111)
+
+    def _paxes_handling_preparation(self):
+
+        if self.twinx:
+            self._pax = self._ax.twinx()
+        else:
+            self._pax = self._ax
+
+    def _generate_emission_part(self):
+
+        lams = [self.lam_noint, self.lam_escat]
+        weights = [self.weights_noint, self.weights_escat]
+        colors = ["black", "grey"]
+
+
+        for zi in xrange(1, self.zmax+1):
+            mask = self.line_out_infos.atomic_number.values == zi
+            lams.append((csts.c.cgs / (self.line_out_nu[mask] * units.Hz)).to(units.AA))
+            weights.append(self.line_out_L[mask] * units.erg / self.mdl.time_of_simulation)
+            colors.append(self.cmap(float(zi) / float(self.zmax)))
+
+        Lnorm = 0
+        for w in weights:
+            Lnorm += np.sum(w)
+
+        ret = self.ax.hist(lams, bins=self.bins, stacked=True, histtype="stepfilled",
+                      normed=True, weights=weights)
+
+        for i, col in enumerate(ret[-1]):
+            for reti in col:
+                reti.set_facecolor(colors[i])
+                reti.set_edgecolor(colors[i])
+                reti.set_linewidth(0)
+                reti.xy[:, 1] *= Lnorm
+
+        self.ax.plot(self.mdl.spectrum_virtual.wavelength,
+                self.mdl.spectrum_virtual.luminosity_density_lambda,
+                color="blue", drawstyle="steps-post", lw=0.5)
+
+    def _generate_absorption_part(self):
+
+        lams = []
+        weights = []
+        colors = []
+
+        for zi in xrange(1, self.zmax+1):
+            mask = self.line_in_infos.atomic_number.values == zi
+            lams.append((csts.c.cgs / self.line_in_nu[mask]).to(units.AA))
+            weights.append(self.line_in_L[mask] * units.erg / self.mdl.time_of_simulation)
+            colors.append(self.cmap(float(zi) / float(self.zmax)))
+
+        Lnorm = 0
+        for w in weights:
+            Lnorm -= np.sum(w)
+
+        ret = self.pax.hist(lams, bins=self.bins, stacked=True, histtype="stepfilled",
+                    normed=True, weights=weights)
+
+        for i, col in enumerate(ret[-1]):
+            for reti in col:
+                reti.set_facecolor(colors[i])
+                reti.set_edgecolor(colors[i])
+                reti.set_linewidth(0)
+                reti.xy[:, 1] *= Lnorm
+
+    def _generate_and_add_colormap(self):
+
+        custcmap = matplotlib.colors.ListedColormap([self.cmap(float(i) / float(self.zmax))
+                                                    for i in xrange(1, self.zmax+1)])
+        bounds = np.arange(self.zmax+1) + 0.5
+        norm = matplotlib.colors.Normalize(vmin=1, vmax=self.zmax+1)
+        mappable = cm.ScalarMappable(norm=norm, cmap=custcmap)
+        mappable.set_array(np.linspace(1, self.zmax + 1, 256))
+        labels = [inv_elements[zi].capitalize() for zi in xrange(1, self.zmax+1)]
+
+        mainax = self.ax
+        cbar = plt.colorbar(mappable, ax=mainax)
+        cbar.set_ticks(bounds)
+        cbar.set_ticklabels(labels)
+
+    def _generate_and_add_legend(self):
+
+        bpatch = patches.Patch(color="black", label="photosphere")
+        gpatch = patches.Patch(color="grey", label="e-scattering")
+        bline = lines.Line2D([], [], color="blue", label="virtual spectrum")
+        self.ax.legend(handles=[bline, gpatch, bpatch])
+
+    def _axis_handling_label_rescale(self):
+        if self.ylim is None:
+            self.ax.autoscale(axis="y")
+        else:
+            self.ax.set_ylim(self.ylim)
+
+        self._ylim = self.ax.get_ylim()
+
+        if self.xlim is None:
+            self.ax.autoscale(axis="x")
+        else:
+            self.ax.set_xlim(self.xlim)
+
+        self._xlim = self.ax.get_xlim()
+
+        if self.twinx:
+            self.pax.set_ylim([-self.ylim[-1], -self.ylim[0]])
+            self.pax.set_yticklabels([])
+        else:
+            self.pax.set_ylim([-self.ylim[-1], self.ylim[-1]])
+        self.pax.set_xlim(self.xlim)
+
+        self.ax.set_xlabel(r"$\lambda$ [$\mathrm{\AA}$]")
+        self.ax.set_ylabel(r"$L_{\mathrm{\lambda}}$ [$\mathrm{erg\,s^{-1}\,\AA^{-1}}$]")
